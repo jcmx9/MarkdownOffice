3e06f46b195ebc398d12335eba7b8cc844a7c397
jcmx9 <jcmx9@tutamail.com>
2026-04-05 00:32:13 +0200

    feat: add TypstBridge wrapper for compile and discoverInputs

---
+++ lib/core/typst_bridge.dart
@@ -0,0 +1,45 @@
+import 'dart:typed_data';
+import 'package:typst_flutter/typst_flutter.dart' as typst;
+// ignore: implementation_imports
+import 'package:typst_flutter/src/rust/api/typst_compiler.dart' as rust;
+import '../models/input_field.dart';
+
+class TypstBridge {
+  static bool _initialized = false;
+
+  static Future<void> init() async {
+    if (!_initialized) {
+      await typst.TypstFlutter.init();
+      _initialized = true;
+    }
+  }
+
+  static Future<List<InputField>> discoverInputs(String templateSource) async {
+    await init();
+    final results = await rust.discoverInputs(source: templateSource);
+    return results
+        .map(
+          (r) => InputField(
+            name: r.name,
+            required: r.required_,
+            defaultValue: r.defaultValue,
+          ),
+        )
+        .toList();
+  }
+
+  static Future<Uint8List> compile({
+    required String templateSource,
+    required Map<String, String> inputs,
+    List<typst.FontSource> fonts = const [],
+    List<typst.ExtraFileSource> files = const [],
+  }) async {
+    await init();
+    return typst.TypstFlutter.compileString(
+      template: templateSource,
+      inputs: inputs,
+      fonts: fonts,
+      extraFiles: files,
+    );
+  }
+}
